ae8727b90f3762bd3cc108320311c252db430498
shrishail735 <kumbharshrishail735@gmail.com>
2024-02-03 03:57:17 +0530

    improved navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { ReviewCardComponent } from './shared/review-card/review-card.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
+  
     BrowserAnimationsModule
   ],
   providers: [],
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+mobileMenu=false;
+
 
 }
